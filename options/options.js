911d99a10d78c746ec865d6db775fe6471e4d2c3
Crowdbotics <team@crowdbotics.com>
2023-09-30 19:14:06 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "workout-tracker-43676",
+  url: "https://workout-tracker-43676.botics.co",
+  api: "https://workout-tracker-43676.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
